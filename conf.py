a8fc615da114f71ac2dcfb3fa86320669ddda2a4
Mariatta <Mariatta@users.noreply.github.com>
2017-12-05 10:04:50 -0800

    Show the "Fork me on GitHub" banner.

--- conf.py
+++ conf.py
@@ -115,7 +115,12 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'github_user': 'pycascades',
+    'github_repo': 'welcome-wagon-2018',
+    'github_banner': True,
+    'github_type': 'star'
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
